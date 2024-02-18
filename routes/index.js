9796c1de6f2c994417966bc2f789be76b6805f03
manishchandrani <manishsaikrishna.c@gmail.com>
2024-02-18 17:56:25 -0600

    Added title for the Contact Database App

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Contact Database App', info: 'Welcome to the Contact Database Application! This is simple Contact Database' });
 });
 
 module.exports = router;
