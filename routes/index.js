692a8e57ff892993b03c76709892e8447def2a86
manishchandrani <manishsaikrishna.c@gmail.com>
2024-02-18 20:42:54 -0600

    Added contact information

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Contact Database App', info: 'Welcome to the Contact Database Application! This is simple Contact Database' });
+  res.render('index', { title: 'Contact Database App', info: 'Welcome to the Contact Database Application!', name: 'Manish Chandrani', email: 'mchandrani@hawk.iit.edu' });
 });
 
 module.exports = router;
