a64c731dfae963ff0efed764fdcc3e16ceff8ee7
manishchandrani <manishsaikrishna.c@gmail.com>
2024-02-18 21:10:54 -0600

    naming change in all contacts route handler

--- routes/contact.js
+++ routes/contact.js
@@ -6,7 +6,7 @@ const contactUtils = require('../models/contactUtil');
 // Route handler for displaying all contacts
 router.get('/', (req, res) => {
   const contacts = contactUtils.readContactsFromFile();
-  res.render('contacts/index', { contacts });
+  res.render('contacts/allcontacts', { contacts });
 });
 
 // Route handler for showing the form to create a new contact
