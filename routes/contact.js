52a9f28398448737b82fbd112470569b5fd1ed43
manishchandrani <manishsaikrishna.c@gmail.com>
2024-02-29 18:39:20 -0600

    changed the route handler for displaying all contacts

--- routes/contact.js
+++ routes/contact.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
 const Contact = require('../models/contact');
-const contactUtils = require('../models/contactUtil');
+const { body, validationResult } = require('express-validator');
+const contactUtils = require('../models/contactUtilSQLite');
 
 // Route handler for displaying all contacts
 router.get('/', (req, res) => {
-  const contacts = contactUtils.readContactsFromFile();
+  const contacts = contactUtils.readContactsFromDatabase();
   res.render('contacts/allcontacts', { contacts });
 });
 
 // Route handler for showing the form to create a new contact
 router.get('/new', (req, res) => {
   res.render('contacts/new');
+});
+
+// Route handler for displaying a single contact
+router.get('/:id', (req, res) => {
+  const contactId = req.params.id;
+  const contact = contactUtils.getContactByIdFromDatabase(contactId);
+  if (!contact) {
+    return res.status(404).send('Contact not found');
+  }
+  res.render('contacts/detail', { contact });
 });
 
 // Validation middleware
@@ -36,36 +46,18 @@ router.post('/', validateContact, (req, res) => {
   newContact.generateContactId();
   newContact.setDateTime();
 
-  const contacts = contactUtils.readContactsFromFile();
-  contactUtils.addContact(newContact, contacts);
+  contactUtils.addContactToDatabase(newContact);
   res.redirect('/contacts');
-});
-
-
-// Route handler for displaying a single contact
-router.get('/:id', (req, res) => {
-  const contacts = contactUtils.readContactsFromFile();
-  const contactId = req.params.id;
-  try {
-    const contact = Contact.getContactById(contactId, contacts);
-    res.render('contacts/detail', { contact });
-  } catch (error) {
-    console.error('Error retrieving contact:', error);
-    res.status(404).send('Contact not found');
-  }
 });
 
 // Route handler for editing a contact
 router.get('/:id/editpage', (req, res) => {
-  const contacts = contactUtils.readContactsFromFile();
   const contactId = req.params.id;
-  try {
-    const contact = Contact.getContactById(contactId, contacts);
-    res.render('contacts/edit', { contact });
-  } catch (error) {
-    console.error('Error retrieving contact for editing:', error);
-    res.status(404).send('Contact not found');
+  const contact = contactUtils.getContactByIdFromDatabase(contactId);
+  if (!contact) {
+    return res.status(404).send('Contact not found');
   }
+  res.render('contacts/edit', { contact });
 });
 
 // Route handler for updating an existing contact with validation
@@ -73,41 +65,24 @@ router.post('/:id/edit', validateContact, (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const contactId = req.params.id;
-    const contacts = contactUtils.readContactsFromFile();
-    try {
-      const contact = Contact.getContactById(contactId, contacts);
-      return res.render('contacts/edit', { contact, msg: errors.array() });
-    } catch (error) {
-      console.error('Error retrieving contact for editing:', error);
-      return res.status(404).send('Contact not found');
-    }
+    const contact = contactUtils.getContactByIdFromDatabase(contactId);
+    return res.render('contacts/edit', { contact, msg: errors.array() });
   }
 
   const contactId = req.params.id;
   const { firstName, lastName, emailAddress, notes } = req.body;
   const updatedContact = new Contact(firstName, lastName, emailAddress, notes);
-  updatedContact.generateContactId(); 
-  updatedContact.setDateTime(); 
-  const contacts = contactUtils.readContactsFromFile();
-  contactUtils.updateContact(contactId, updatedContact, contacts);
+  updatedContact.setDateTime();
+  contactUtils.updateContactInDatabase(contactId, updatedContact);
   res.redirect(`/contacts`);
 });
-
 
 // Route handler for deleting a contact
 router.post('/:id/delete', (req, res) => {
   const contactId = req.params.id;
-  const deletionSuccessful = contactUtils.deleteContact(contactId);
-  if (deletionSuccessful) {
-    res.redirect('/contacts');
-  } else {
-    res.status(404).send('Contact not found');
-  }
+  contactUtils.deleteContactFromDatabase(contactId);
+  res.redirect('/contacts');
 });
-
-
-
-
 
 
 module.exports = router;
